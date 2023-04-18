d4dc2303ef3c9c4fc70551c46e991f02e111c507
Shylex96 <77785532+Shylex96@users.noreply.github.com>
2023-04-18 13:29:19 +0200

    Add files via upload

---
+++ EjerciciosJS/Avanzados/exercise7.js
@@ -0,0 +1,25 @@
+const square1 = document.getElementById('cuadrado1');
+const square2 = document.getElementById('cuadrado2');
+const square3 = document.getElementById('cuadrado3');
+
+function verifyNumber() {
+    const inputRed = document.getElementById('input1').value;
+    const inputGreen = document.getElementById('input2').value;
+    const inputBlue = document.getElementById('input3').value;
+
+    if (inputRed == "" || inputGreen == "" || inputBlue == "") {
+
+        // document.getElementById("avisos").innerHTML = "No puede haber campos sin rellenar.";
+        alert("No puede haber campos sin rellenar");
+
+    } else if (((inputRed < 0 || inputRed > 255) || (inputGreen < 0 || inputGreen > 255) || (inputBlue < 0 || inputBlue > 255))) {
+
+        // document.getElementById("avisos").innerHTML = "Los campos deben contener números comprendidos entre 0 y 255.";
+        alert("Los campos deben contener números comprendidos entre 0 y 255.");
+
+    } else {
+
+        let colorFondo = "rgb(" + inputRed + ", " + inputGreen + ", " + inputBlue + ")";
+        document.body.style.backgroundColor = colorFondo;
+    }
+}
